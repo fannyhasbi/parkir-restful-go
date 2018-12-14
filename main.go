033782f705cc6d02a9137a768e1887ad0e6932ef
fannyhasbi <fnyhsbi@gmail.com>
2018-12-14 14:51:36 +0700

    Migrate to MySQL

--- main.go
+++ main.go
@@ -1,44 +1,28 @@
 package main
 
 import (
-  "database/sql"
-  "fmt"
-  _ "github.com/lib/pq"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
-const (
-  DB_USER     = "postgres"
-  DB_PASSWORD = "hasbi12345"
-  DB_NAME     = "parking"
-)
+const PORT = ":8080"
 
-func main(){
-  dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s port=5433 sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
-  db, err := sql.Open("postgres", dbinfo)
-  checkErr(err)
-  defer db.Close()
-
-  fmt.Println("# SELECT Database")
-  rows, err := db.Query("SELECT * FROM owner")
-  checkErr(err)
-
-  fmt.Println("oid | nama | nim")
-
-  for rows.Next(){
-    var oid int
-    var nama string
-    var nim string
-
-    err = rows.Scan(&oid, &nama, &nim)
-    checkErr(err)
-
-    fmt.Printf("%v | %11v | %v\n", oid, nama, nim)
-  }
-
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	response := "This is web service for React Parking System application"
+	fmt.Fprint(w, response)
 }
 
-func checkErr(err error){
-  if err != nil {
-    panic(err)
-  }
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", handleIndex).Methods("GET", "POST")
+
+	http.Handle("/", router)
+
+	fmt.Printf("Connected to port %v", PORT)
+	log.Fatal(http.ListenAndServe(PORT, router))
 }
